9aa7e086f8db4b38096675023999b377daca3037
DylanKing2018 <37106032+DylanKing2018@users.noreply.github.com>
2018-11-15 16:07:58 +0000

    Files made. Product and Coin finished.

---
+++ Coin.java
@@ -0,0 +1,38 @@
+/**
+ * Coin
+ */
+public class Coin {
+
+	private double value;
+	private String name;
+
+	/**
+     * Constructs a coin.
+     * @param value the monetary value of the coin.
+     * @param name the name of the coin
+	*/
+	public Coin(double value, String name) 
+  	{ 
+		this.value = aValue; 
+		this.name = aName;
+	}
+	
+	/**
+	 * @return the value
+	 */
+	public double getValue() {
+		return value;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
---
+++ Product.java
@@ -0,0 +1,54 @@
+/**
+ * Product
+ */
+public class Product {
+
+	private String description;
+	private double price;
+	private int quantity;
+
+	/**
+     * Constructs a Product object
+     * @param description the description of the product
+     * @param price the price of the product
+	*/
+	public Product(String description, double price)
+	{  
+		this.description = description;
+		this.price = price;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Description:\t%s\nPrice:\t%.2f\nQuantity Available:\t%d",
+							 description, price, quantity);
+	}
+}
