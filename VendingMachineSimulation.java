3ce9743720fb63a9c4f648ec92f4ad7f3c296971
Shwingbad <38210042+Shwingbad@users.noreply.github.com>
2018-11-16 10:04:34 +0000

    Start on simulation

--- VendingMachineSimulation.java
+++ VendingMachineSimulation.java
@@ -6,6 +6,12 @@ public class VendingMachineSimulation
 
 	public static void main(String[] args)
 	{
+		// Make the initial menu for the machine.
+		VendingMachineMenu menu = new VendingMachineMenu();
+
+		//Run the 
+		menu.run();
+
 		
 	}
 }
