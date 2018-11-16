b298b621b89516b47f05f02cf16a40b4f7cdff14
LouiseMadden <37147382+LouiseMadden@users.noreply.github.com>
2018-11-16 10:05:14 +0000

    Started VendingMachine

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,8 +1,16 @@
-/**
- * VendingMachine
- */
+
+
 public class VendingMachine
 {
+    private ArrayList<Product> products;
+    private ArrayList<Integer> prodQuantity;
+    private ArrayList<Coin> coins;
+    private ArrayList<Integer> coinQuantity;
+    private ArrayList<Coin> currentCoins;
+    private double currentBalance;
+    
+    public VendingMachine() 
+    {
 
-	
+    }
 }
